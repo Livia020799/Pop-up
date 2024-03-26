b2fc62b60d3bc6d77e39826fbddd89cd5c0cc163
Livia020799 <146645775+Livia020799@users.noreply.github.com>
2024-03-26 20:05:40 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('emergencyButton').addEventListener('click', function() {
- // Show the modal
- document.getElementById('giphyModal').style.display = 'flex';
-
- // Display an alert after 2 seconds
- setTimeout(function() {
-    alert('Love you');
- }, 1000);
-});
