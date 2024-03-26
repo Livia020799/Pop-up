3055156e4efeb36f2776e48874b184908cde3f67
Livia020799 <146645775+Livia020799@users.noreply.github.com>
2024-03-26 20:15:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('emergencyButton').addEventListener('click', function() {
+    // Show the modal
+    document.getElementById('giphyModal').style.display = 'flex';
+
+    // Display an alert after 2 seconds
+    setTimeout(function() {
+        alert('Love you');
+    }, 1000);
+});
